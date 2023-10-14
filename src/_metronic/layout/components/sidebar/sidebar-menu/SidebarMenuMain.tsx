b3b16c50beff1097fc14f8aa61d3b33f514dc7bd
Gpalacios <gpobregon1@gmail.com>
2023-10-14 13:35:22 -0600

    [INIT]

---
+++ src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react'
+import {useIntl} from 'react-intl'
+import {KTSVG} from '../../../../helpers'
+import {SidebarMenuItemWithSub} from './SidebarMenuItemWithSub'
+import {SidebarMenuItem} from './SidebarMenuItem'
+
+const SidebarMenuMain = () => {
+  const intl = useIntl()
+
+  return (
+    <>
+      <SidebarMenuItem
+        to='/dashboard'
+        icon='/media/icons/duotune/art/art002.svg'
+        title={intl.formatMessage({id: 'MENU.DASHBOARD'})}
+        fontIcon='bi-app-indicator'
+      />
+      <SidebarMenuItem
+        to='/builder'
+        icon='/media/icons/duotune/general/gen019.svg'
+        title='Layout Builder'
+        fontIcon='bi-layers'
+      />
+    </>
+  )
+}
+
+export {SidebarMenuMain}
